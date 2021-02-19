bd8c6f11db177d4ce38c1249f67139025e7d8192
Christina O'Donnell <christina.odonnell@protonmail.com>
2021-02-19 13:25:57 +0000

    Cleanup

--- kfighter/code/linux_kfighter.cpp
+++ kfighter/code/linux_kfighter.cpp
@@ -12,7 +12,7 @@ global Window win;
 global GC gc;
 global Atom wmDeleteWindow;
 global bool running = true;
-global int winWidth, winHeight;
+global int winWidth = 800, winHeight = 600;
 global XImage* backBuffer;
 
 void linuxErrorMessage(char const* str) {
@@ -32,8 +32,7 @@ void linuxDrawPattern(XImage* b) {
 void linuxResizeBackBuffer(int width, int height) {
 	backBuffer->width = width;
 	backBuffer->height = height;
-	backBuffer->data = (char*)realloc(
-		backBuffer->data, 4 * width * height);
+	backBuffer->data = (char*)realloc(backBuffer->data, 4 * width * height);
 	if (backBuffer->data == NULL) {
 		linuxErrorMessage("Could not resize back buffer.");
 		exit(1);
@@ -50,14 +49,6 @@ void linuxHandleEvent(XEvent ev) {
 	} break;
 	case Expose: {
 	draw:
-		/*int mx = winWidth/2;
-		int my = winHeight/2;
-		XSetForeground(display, gc, 0x00FFFFFF);
-		XFillRectangle(display, pm, gc, 0, 0, winWidth, winHeight);
-		XSetForeground(display, gc, 0xFF0000FF);
-		XFillRectangle(display, pm, gc, mx-10, my-10, 20, 20);
-		XDrawString(display, pm, gc, mx-15, my+30, "hello", 5);
-		XCopyArea(display, pm, win, gc, 0, 0, winWidth, winHeight, 0, 0);*/
 		linuxDrawPattern(backBuffer);
 		XPutImage(display, win, gc, backBuffer, 0, 0, 0, 0, winWidth, winHeight);
 	} break;
@@ -101,7 +92,7 @@ int main(int argc, char const* const* argv) {
 	swa.colormap = XCreateColormap(display, root, vinfo.visual, AllocNone);
 	win = XCreateWindow(
 		display, root,
-		0, 0, 800, 600, 1, 32, InputOutput, vinfo.visual,
+		0, 0, winWidth, winHeight, 1, 32, InputOutput, vinfo.visual,
 		CWBackPixel | CWBorderPixel | CWBitGravity | CWEventMask | CWColormap, &swa);
 	XStoreName(display, win, "KFighter");
 	wmDeleteWindow = XInternAtom(display, "WM_DELETE_WINDOW", False);
@@ -110,27 +101,6 @@ int main(int argc, char const* const* argv) {
 	XGCValues xgcvals = {0};
 	xgcvals.graphics_exposures = False;
 	gc = XCreateGC(display, win, GCGraphicsExposures, &xgcvals);
-	winWidth = 800;
-	winHeight = 600;
-	//pm = XCreatePixmap(display, win, 800, 600, 32);
-	//backBuffer = {0};
-	//backBuffer.width = winWidth;
-	//backBuffer.height = winHeight;
-	//backBuffer.format = XYBitmap;
-	//backBuffer.data = (char*)malloc(4 * winHeight * winWidth);
-	//backBuffer.byte_order = LSBFirst;
-	//backBuffer.bitmap_unit = 32;
-	//backBuffer.bitmap_bit_order = LSBFirst;
-	//backBuffer.bitmap_pad = 32;
-	//backBuffer.bytes_per_line = 4 * winWidth;
-	//backBuffer.bits_per_pixel = 32;
-	//backBuffer.red_mask = 0x00FF0000;
-	//backBuffer.green_mask = 0x0000FF00;
-	//backBuffer.blue_mask = 0x000000FF;
-	//if (!XInitImage(&backBuffer)) {
-	//	linuxErrorMessage("Could not create back buffer.");
-	//	exit(1);
-	//}
 	backBuffer = XCreateImage(display, vinfo.visual, 32, ZPixmap, 0,
 		(char*)malloc(4 * winHeight * winWidth),
 		winWidth, winHeight, 32, 4 * winWidth);

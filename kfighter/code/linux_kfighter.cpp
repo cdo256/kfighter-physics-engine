bed98ca2e87d9392f7211e9fe9c7b561a3b47b5a
Christina O'Donnell <christina.odonnell@protonmail.com>
2021-02-19 13:23:42 +0000

    Resizing now works!

--- kfighter/code/linux_kfighter.cpp
+++ kfighter/code/linux_kfighter.cpp
@@ -5,23 +5,40 @@
 #include <X11/Xatom.h>
 
 #include "kfighter_global.h"
+#include "kfighter_maths.h"
 
 global Display* display;
 global Window win;
 global GC gc;
 global Atom wmDeleteWindow;
 global bool running = true;
-global Pixmap pm;
 global int winWidth, winHeight;
+global XImage* backBuffer;
 
 void linuxErrorMessage(char const* str) {
 	fprintf(stderr, "Fatal error: %s\n", str);
 }
 
-void linuxResizePixmap(int width, int height) {
-	XFreePixmap(display, pm);
-	pm = XCreatePixmap(display, win, width, height, 32);
-	//XResizeWindow(display, win, width, height);
+void linuxDrawPattern(XImage* b) {
+	char* line = b->data;
+	for (int y = 0; y < b->height; y++) {
+		for (int x = 0; x < b->width; x++) {
+			((u32*)line)[x] = (x ^ y) | 0xFF000000;
+		}
+		line += b->bytes_per_line;
+	}
+}
+
+void linuxResizeBackBuffer(int width, int height) {
+	backBuffer->width = width;
+	backBuffer->height = height;
+	backBuffer->data = (char*)realloc(
+		backBuffer->data, 4 * width * height);
+	if (backBuffer->data == NULL) {
+		linuxErrorMessage("Could not resize back buffer.");
+		exit(1);
+	}
+	backBuffer->bytes_per_line = 4 * width;
 	winWidth = width;
 	winHeight = height;
 }
@@ -33,18 +50,20 @@ void linuxHandleEvent(XEvent ev) {
 	} break;
 	case Expose: {
 	draw:
-		int mx = winWidth/2;
+		/*int mx = winWidth/2;
 		int my = winHeight/2;
 		XSetForeground(display, gc, 0x00FFFFFF);
-		XFillRectangle(display, win, gc, 0, 0, winWidth, winHeight);
+		XFillRectangle(display, pm, gc, 0, 0, winWidth, winHeight);
 		XSetForeground(display, gc, 0xFF0000FF);
 		XFillRectangle(display, pm, gc, mx-10, my-10, 20, 20);
 		XDrawString(display, pm, gc, mx-15, my+30, "hello", 5);
-		//XCopyArea(display, pm, win, gc, 0, 0, winWidth, winHeight, 0, 0);
+		XCopyArea(display, pm, win, gc, 0, 0, winWidth, winHeight, 0, 0);*/
+		linuxDrawPattern(backBuffer);
+		XPutImage(display, win, gc, backBuffer, 0, 0, 0, 0, winWidth, winHeight);
 	} break;
 	case ConfigureNotify: {
 		XConfigureEvent e = ev.xconfigure;
-		linuxResizePixmap(e.width, e.height);
+		linuxResizeBackBuffer(e.width, e.height);
 		goto draw;
 	} break;
 	case ClientMessage: {
@@ -93,7 +112,32 @@ int main(int argc, char const* const* argv) {
 	gc = XCreateGC(display, win, GCGraphicsExposures, &xgcvals);
 	winWidth = 800;
 	winHeight = 600;
-	pm = XCreatePixmap(display, win, 800, 600, 32);
+	//pm = XCreatePixmap(display, win, 800, 600, 32);
+	//backBuffer = {0};
+	//backBuffer.width = winWidth;
+	//backBuffer.height = winHeight;
+	//backBuffer.format = XYBitmap;
+	//backBuffer.data = (char*)malloc(4 * winHeight * winWidth);
+	//backBuffer.byte_order = LSBFirst;
+	//backBuffer.bitmap_unit = 32;
+	//backBuffer.bitmap_bit_order = LSBFirst;
+	//backBuffer.bitmap_pad = 32;
+	//backBuffer.bytes_per_line = 4 * winWidth;
+	//backBuffer.bits_per_pixel = 32;
+	//backBuffer.red_mask = 0x00FF0000;
+	//backBuffer.green_mask = 0x0000FF00;
+	//backBuffer.blue_mask = 0x000000FF;
+	//if (!XInitImage(&backBuffer)) {
+	//	linuxErrorMessage("Could not create back buffer.");
+	//	exit(1);
+	//}
+	backBuffer = XCreateImage(display, vinfo.visual, 32, ZPixmap, 0,
+		(char*)malloc(4 * winHeight * winWidth),
+		winWidth, winHeight, 32, 4 * winWidth);
+	if (!backBuffer) {
+		linuxErrorMessage("Could not create back buffer.");
+		exit(1);
+	}
 	while (running) {
 		XEvent e;
 		XNextEvent(display, &e);
